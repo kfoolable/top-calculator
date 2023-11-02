ab7a9280245fe80aa5330deea27ba32111dd3f8d
Gillian <50127731+kfoolable@users.noreply.github.com>
2023-11-02 18:17:22 +0800

    Updated logic for array insertion

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const btnCon = document.querySelector("div.buttons");
 
 const btnNumbers = btnCon.querySelectorAll("button.dsp");
 const btnOperators = btnCon.querySelectorAll("button.op");
+const btnDot = btnCon.querySelector("button.dot");
 
 const btnEquals = btnCon.querySelector(".equals");
 
@@ -18,6 +19,8 @@ let result = "";
 
 let isFirstInput = true;
 let isSecondInput = false;
+
+let myArray = [];
 
 function add(num1, num2) {
   return num1 + num2;
@@ -53,7 +56,7 @@ function operate(op, first, second) {
 
 btnNumbers.forEach((button) => {
   button.addEventListener("click", () => {
-    if (isFirstInput) {
+    if (operator === null) {
       firstInput += button.textContent;
       display.textContent = firstInput;
     } else {
@@ -66,12 +69,68 @@ btnNumbers.forEach((button) => {
 btnOperators.forEach((button) => {
   button.addEventListener("click", () => {
     if (firstInput !== "") {
+      if (operator && secondInput !== "") {
+        // If there's already an operator and second input, calculate and reset
+        const calculated = operate(operator, firstInput, secondInput);
+        myArray.push(calculated);
+        firstInput = String(calculated);
+        secondInput = "";
+        //console.log(calculated);
+        display.textContent = calculated;
+      }
       operator = button.textContent;
-      isFirstInput = false;
+      //display.textContent = secondInput;
     }
   });
 });
 
-btnEquals.addEventListener("click", () => {
-  display.textContent = operate(operator, firstInput, secondInput);
+btnDot.addEventListener("click", () => {
+  if (isFirstInput) {
+    if (!firstInput.includes(".")) {
+      firstInput += ".";
+      display.textContent = firstInput;
+    }
+  } else if (isSecondInput) {
+    if (!secondInput.includes(".")) {
+      secondInput += ".";
+      display.textContent = secondInput;
+    }
+  }
 });
+
+btnEquals.addEventListener("click", () => {
+  if (firstInput !== "" && operator !== null && secondInput !== "") {
+    const calculated = operate(operator, firstInput, secondInput);
+    myArray.push(calculated);
+    result = calculated;
+    display.textContent = result;
+    firstInput = String(result);
+    secondInput = "";
+    operator = null;
+  }
+});
+
+// btnNumbers.forEach((button) => {
+//   button.addEventListener("click", () => {
+//     if (isFirstInput) {
+//       firstInput += button.textContent;
+//       display.textContent = firstInput;
+//     } else {
+//       secondInput += button.textContent;
+//       display.textContent = secondInput;
+//     }
+//   });
+// });
+
+// btnOperators.forEach((button) => {
+//   button.addEventListener("click", () => {
+//     if (firstInput !== "") {
+//       operator = button.textContent;
+//       isFirstInput = false;
+//     }
+//   });
+// });
+
+// btnEquals.addEventListener("click", () => {
+//   display.textContent = operate(operator, firstInput, secondInput);
+// });
